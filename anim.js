16d8c36c51089ddc149e4137edaf551de0a31412
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-09-21 01:40:15 -0500

    update

---
+++ anim.js
@@ -0,0 +1,17 @@
+
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+setInterval(updateLyrics, 1000);
+
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
